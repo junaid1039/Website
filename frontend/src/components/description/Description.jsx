a8ddf584d60c32cbe17881ce360a714c36aaf16f
junaid1039 <ssmbag103955@gmail.com>
2024-10-25 09:13:09 +0500

    newupdate

--- frontend/src/components/description/Description.jsx
+++ frontend/src/components/description/Description.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './description.css';
 
-const Description = () => {
+const Description = (description) => {
 
     return (
         <div className="description">
@@ -9,12 +9,9 @@ const Description = () => {
                 <div className="des-nav-box">Description</div>
             </div>
             <div className="des-description">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus blanditiis dolorem nam aliquid eos repudiandae odit eum quidem in veniam, animi voluptate fugiat perferendis quia distinctio,
-                    vitae omnis corrupti tempore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid sit animi odit adipisci eius quaerat dolorum commodi earum facere, dolor, provident sunt blanditiis
-                    nostrum voluptatibus ex? Illo sapiente temporibus fugiat.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus neque, earum nulla debitis voluptas a excepturi mollitia maxime voluptatem alias hic, aperiam quos. Explicabo, quod! Officiis
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ratione cumque quidem, provident accusantium illo magnam temporibus sapiente magni doloremque delectus suscipit iusto quaerat labore
-                    vel placeat quisquam distinctio corrupti? inventore doloribus velit minus.</p>
+               <p>
+                {description}
+               </p>
             </div>
         </div>
     )
