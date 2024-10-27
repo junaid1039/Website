050eac301e276936a475d897d9a15e28bccb0ba4
junaid1039 <ssmbag103955@gmail.com>
2024-10-27 12:00:00 +0500

    .

--- frontend/src/components/c-items/Citems.jsx
+++ frontend/src/components/c-items/Citems.jsx
@@ -4,7 +4,7 @@ import './citem.css';
 export const Citems = (props) => {
   return (
     <div className="citem">
-        <img src={props.image}/>
+        <img src={props.image} alt='category'/>
         <p>{props.name}</p>
     </div>
   )
