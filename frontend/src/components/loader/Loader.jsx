6a6205f740aa7850d6f5d9c574ed6bd7b7f0bdd6
junaid1039 <ssmbag103955@gmail.com>
2024-11-12 12:07:19 +0500

    loader fix

--- frontend/src/components/loader/Loader.jsx
+++ frontend/src/components/loader/Loader.jsx
@@ -1,17 +1,16 @@
-// Loader.js
 import React from 'react';
 import './loader.css';
 
 const Loader = () => {
     return (
-        <div className="loader-container">
-            <div className="loader">
-                <div className="circle"></div>
-                <div className="circle"></div>
-                <div className="circle"></div>
-                <div className="shadow"></div>
-                <div className="shadow"></div>
-                <div className="shadow"></div>
+        <div className="loader-wrapper">
+            <div className="loading-animation">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="shadow-effect"></div>
+                <div className="shadow-effect"></div>
+                <div className="shadow-effect"></div>
             </div>
             <h2>Loading...</h2>
         </div>
